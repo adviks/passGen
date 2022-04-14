d850a325dd49e5a933a0e5050eb2a308d139f754
Advik <aadvik@duck.com>
2022-04-14 22:20:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let pass1El = document.getElementById("pass1-el")
+let pass2El = document.getElementById("pass2-el")
+let pass3El = document.getElementById("pass3-el")
+let pass4El = document.getElementById("pass4-el")
+
+
+
+function genPass(){
+    pass1El.textContent = "ghaoelapae"
+    pass2El.textContent = "ghaoelapae"
+    pass3El.textContent = "ghaoelapae"
+    pass4El.textContent = "ghaoelapae"
+}
